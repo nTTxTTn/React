64875feb90f62ea3550230365fa5f2f62a7811c3
nTTxTTn <seungheun99@naver.com>
2024-07-10 00:59:36 +0900

    20240710
    test2

---
+++ my-app/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import Library from "./JSX_TEST/Library";
+import Clock from "./Clock/Clock";
+import CommentList from "./Comment/CommentList";
+
+/*
+------------테스트-------------
+ReactDOM.render(
+    <React.StrictMode>
+        <Library />
+    </React.StrictMode>,
+    document.getElementById('root')
+);*/
+
+/*
+-------------시계-------------
+setInterval(() => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <Clock/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+},1000) */
+
+/*
+------------댓글--------------
+ReactDOM.render(
+    <React.StrictMode>
+        <CommentList/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+*/
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
