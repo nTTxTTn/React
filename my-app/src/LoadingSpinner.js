de8f45c53db1b7a6a4a45b509374788cc77403f6
nobisil <mandae69@naver.com>
2024-08-30 12:47:59 +0900

    darkmode theme로 감싸기

---
+++ my-app/src/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingSpinner() {
+    return (
+        <div className="loading-spinner">
+            <div className="spinner"></div>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
