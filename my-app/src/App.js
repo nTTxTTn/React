db31164a560ca731977394c2b5196b7a0dfcf1fc
nobisil <mandae69@naver.com>
2024-09-11 20:20:27 +0900

    라우팅추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,6 +15,8 @@ import FlashcardView from "./FlashcardView";
 import WordListDetail from "./WordListDetail";
 import WordListPage from "./WordListPage";
 import EditWordList from "./EditWordList";
+import QuizPage from "./QuizPage";
+import QuizResult from "./QuizResult";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
@@ -103,6 +105,8 @@ function AppWithAuth() {
                         <Route path="/wordlist/:id" element={<WordListDetail user={user} />} />
                         <Route path="/words" element={<WordListPage user={user} />} />
                         <Route path="/edit-wordlist/:id" element={<EditWordList />} />
+                        <Route path="/quiz" element={<QuizPage user={user} />} />
+                        <Route path="/quiz-result" element={<QuizResult user={user} />} />
                     </Routes>
                 </main>
             </div>
